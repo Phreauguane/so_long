a15598f34350e60153aa5b46a812be83506a190d
Jules De Meo <jde-meo@c3r3p6.42perpignan.fr>
2024-03-11 14:38:18 +0100

    auto

--- src/main.c
+++ src/main.c
@@ -6,18 +6,31 @@
 /*   By: jde-meo <jde-meo@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 18:39:43 by jde-meo           #+#    #+#             */
-/*   Updated: 2024/03/10 23:54:51 by jde-meo          ###   ########.fr       */
+/*   Updated: 2024/03/11 14:36:40 by jde-meo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
+static void	verif_input(int ac, char **av)
+{
+	int	i;
+	
+	if (ac != 2 || !av[1])
+		exit_handler("Wrong arguments", NULL, NULL);
+	i = ft_strlen(av[1]);
+	if (i < 4)
+		exit_handler("Invalid file", NULL, NULL);
+	if (av[1][i - 4] != '.' || av[1][i - 3] != 'b'
+		|| av[1][i - 2] != 'e' || av[1][i - 1] != 'r')
+		exit_handler("Invalid file", NULL, NULL);
+}
+
 int	main(int ac, char **av)
 {
 	t_game	game;
 
-	if (ac != 2)
-		exit_handler("Wrong arguments", NULL, NULL);
+	verif_input(ac, av);
 	ft_bzero(&game, sizeof(t_game));
 	init_game(&game, av[1]);
 	return (0);
